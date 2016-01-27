2fdd9efbbed59aea304740c3b031cb28e3bbe7ea
jtrimmer <jtrimmer@Xtrimmer-Main>
2016-01-26 19:45:41 -0600

    Added a basic login form to test login server functionality, Added Host and IP address information to the USer object.

--- src/main/java/com/ucmo/chat/Test.java
+++ src/main/java/com/ucmo/chat/Test.java
@@ -12,11 +12,6 @@ package com.ucmo.chat;
 public class Test {
     
     public static void main(String[] args) throws Exception{
-        ActiveUsers.addUser("VictoriaW", new User("VictoriaW"));
-        ActiveUsers.addUser("JeffT", new User("JeffT"));
-        ActiveUsers.addUser("MauricioB", new User("MauricioB"));
-        ActiveUsers.addUser("JessicaB", new User("JessicaB"));
-        ActiveUsers.addUser("EngelbertHumperdinck", new User("EngelbertHumperdinck"));
         
         System.out.println(ActiveUsers.getJsonString());
     }
